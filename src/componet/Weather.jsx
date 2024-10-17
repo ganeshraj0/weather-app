56d9d734a8c27b2c6c3a5d7924f1670b23087223
ganeshraj0 <ganesh636164@gmail.com>
2024-10-17 08:43:37 -0700

    weather-app

--- src/componet/Weather.jsx
+++ src/componet/Weather.jsx
@@ -66,8 +66,8 @@ const Weather = () => {
   }, []);
 
   return (
-    <div className="weather place-self-center p-10 rounded-xl items-center flex flex-col w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg">
-        <div className="flex items-center gap-[12px] sm:gap-[2px]">
+    <div className="weather place-self-center p-10 sm:p-1 rounded-xl items-center flex flex-col w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg">
+        <div className="flex items-center gap-[12px]">
         <input
   ref={inputRef}
   className="h-12 sm:h-14 rounded-full border-none outline-none 
